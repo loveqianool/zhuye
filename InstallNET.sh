d8b2f46f3b2894ace3839605a2d6c4689605d083
zhuYe <loveqianool@users.noreply.github.com>
2020-08-13 19:16:59 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -326,7 +326,7 @@ if [[ "$SpikCheckDIST" == '0' ]]; then
 fi
 
 [[ "$ddMode" == '1' ]] && {
-  export SSL_SUPPORT='https://github.com/MoeClub/MoeClub.github.io/raw/master/lib/wget_udeb_amd64.tar.gz';
+  export SSL_SUPPORT='https://cdn.jsdelivr.net/gh/loveqianool/loveqianool.github.io/wget_udeb_amd64.tar.gz';
   if [[ -n "$tmpURL" ]]; then
     DDURL="$tmpURL"
     echo "$DDURL" |grep -q '^http://\|^ftp://\|^https://';
